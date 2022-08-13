bb10b471b83e5ccc84cfea86acfd4ab9d9a3f8cf
noisyboy25 <dj402and@gmail.com>
2022-08-13 23:38:40 +0300

    Add auth without encoding

--- go.mod
+++ go.mod
@@ -4,28 +4,21 @@ go 1.18
 
 require (
 	github.com/gofiber/fiber/v2 v2.35.0
+	github.com/mattn/go-sqlite3 v1.14.12
 	gorm.io/gorm v1.23.8
 )
 
 require (
-	github.com/glebarez/go-sqlite v1.17.3 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
-	modernc.org/libc v1.16.8 // indirect
-	modernc.org/mathutil v1.4.1 // indirect
-	modernc.org/memory v1.1.1 // indirect
-	modernc.org/sqlite v1.17.3 // indirect
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/glebarez/sqlite v1.4.6
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.38.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220405052023-b1e9470b6e64 // indirect
+	gorm.io/driver/sqlite v1.3.6
 )
--- main.go
+++ main.go
@@ -1,30 +1,49 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"mime"
 	"strconv"
 
-	"github.com/glebarez/sqlite"
 	"github.com/gofiber/fiber/v2"
+	"github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
+	"gorm.io/gorm/logger"
 )
 
 type Todo struct {
-	ID   uint   `json:"id" gorm:"primaryKey; not null"`
-	Text string `json:"text"`
+	ID     uint   `json:"id" gorm:"primaryKey; not null"`
+	Text   string `json:"text"`
+	UserID *uint  `json:"userId"`
+	User   *User  `json:"user"`
+}
+
+type User struct {
+	gorm.Model
+	Username string `json:"username" gorm:"unique"`
+	Password string `json:"password"`
+	Todos    []Todo `json:"todos"`
+}
+
+type AuthPair struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 var db *gorm.DB
 
 func init() {
 	var err error
-	db, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	db, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.AutoMigrate(&Todo{})
+	db.AutoMigrate(&Todo{}, &User{})
 }
 
 func main() {
@@ -74,6 +93,51 @@ func main() {
 			return result.Error
 		}
 		return c.JSON(fiber.Map{"deleteCount": result.RowsAffected})
+	})
+
+	api.Get("/users", func(c *fiber.Ctx) error {
+		users := []User{}
+		result := db.Preload("Todos").Find(&users)
+		if result.Error != nil {
+			return result.Error
+		}
+
+		return c.JSON(fiber.Map{"users": users})
+	})
+
+	auth := app.Group("/auth")
+	auth.Post("/login", func(c *fiber.Ctx) error {
+		a := &AuthPair{}
+		if err := c.BodyParser(a); err != nil {
+			return err
+		}
+
+		user := &User{}
+		result := db.Where("username = ?", a.Username).First(user)
+		if result.Error != nil || user.Password != a.Password {
+			return errors.New("wrong username or password")
+		}
+
+		return c.JSON(fiber.Map{"user": user})
+	})
+	auth.Post("/register", func(c *fiber.Ctx) error {
+		a := &AuthPair{}
+		if err := c.BodyParser(a); err != nil {
+			return err
+		}
+
+		user := &User{Username: a.Username, Password: a.Password}
+		result := db.Create(&user)
+		if result.Error != nil {
+			var sqliteError sqlite3.Error
+			if errors.As(result.Error, &sqliteError) {
+				if errors.Is(sqliteError.ExtendedCode, sqlite3.ErrConstraintUnique) {
+					return errors.New("user already exists")
+				}
+			}
+			return result.Error
+		}
+		return c.JSON(fiber.Map{"user": user})
 	})
 
 	app.Listen(":3000")
